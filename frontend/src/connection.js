5cd4c265afcd322d1a8bd0f1a5c6af6e12b568d3
Ege Metin <egemetin@sabanciuniv.edu>
2022-11-02 10:40:48 +0300

    Update connection.js

--- frontend/src/connection.js
+++ frontend/src/connection.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://ref-be.herokuapp.com";
+const url = "https://referee-be.herokuapp.com";
 //const url = "http://localhost:3001";
 
 axios.defaults.baseURL = url;
