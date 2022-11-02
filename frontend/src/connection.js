52709cee295a4777656366240edfd63be8f2492c
Ege Metin <egemetin@sabanciuniv.edu>
2022-11-02 10:21:47 +0300

    connection

--- frontend/src/connection.js
+++ frontend/src/connection.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://referee-assignment-system-backend.vercel.app/";
+const url = "https://ref-be.herokuapp.com";
 //const url = "http://localhost:3001";
 
 axios.defaults.baseURL = url;
