38ded40629d73f05cd805ebb1921ced0bfb370a6
Ege Metin <egemetin@sabanciuniv.edu>
2022-11-22 12:02:05 +0300

    connection link update for deployment

--- frontend/src/connection.js
+++ frontend/src/connection.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 //const url = "https://referee-be.herokuapp.com";
-const url = "http://localhost:3001";
+// const url = "http://localhost:3001";
+const url = "https://tff-referee-assignment-system.herokuapp.com";
 
 axios.defaults.baseURL = url;
