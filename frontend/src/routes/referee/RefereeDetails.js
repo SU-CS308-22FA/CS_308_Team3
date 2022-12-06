dc8cf20db6134c65d815c0c164700517aa275b8d
omegamert <onurmert@sabanciuniv.edu>
2022-12-07 00:50:37 +0300

    Vote Button is done (Front-End)
    User or Tff member can vote for the referee. (Front-End done)

--- frontend/src/routes/referee/RefereeDetails.js
+++ frontend/src/routes/referee/RefereeDetails.js
@@ -1,3 +1,15 @@
+import {
+    Alert,
+    Button,
+    FormControl,
+    InputAdornment,
+    InputLabel,
+    MenuItem,
+    Select,
+    TextField,
+    ToggleButton,
+    ToggleButtonGroup,
+} from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
@@ -5,6 +17,7 @@ import { useLocation } from "react-router-dom";
 import "./refereeDetails.scss";
 
 export default function RefereeDetails() {
+    const [vote, setVote] = useState("");
     const location = useLocation();
     const {
         state: { name },
@@ -20,7 +33,6 @@ export default function RefereeDetails() {
             if (message === "Found") setReferee(referee);
         });
     }, []);
-
     return (
         <div className="refereeDetailsContainer">
             <div>
@@ -34,6 +46,8 @@ export default function RefereeDetails() {
                     <h3>Years of Experience: {referee.experience}</h3>
                     <h3>License: {referee.license}</h3>
                     <h3>Hometown: {referee.hometown}</h3>
+                    <h3>Score: {referee.score}</h3>
+
                 </div>
             </div>
             <h1>{referee.name}</h1>
@@ -53,6 +67,38 @@ export default function RefereeDetails() {
                         )}
                 </div>
             </div>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">
+                    Vote
+                </InputLabel>
+                <Select
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    value={vote}
+                    label="Vote"
+                    onChange={(event) =>
+                        setVote(event.target.value)
+                    }
+                >
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={6}>6</MenuItem>
+                    <MenuItem value={7}>7</MenuItem>
+                    <MenuItem value={8}>8</MenuItem>
+                    <MenuItem value={9}>9</MenuItem>
+                    <MenuItem value={10}>10</MenuItem>
+                </Select>
+                <Button onClick={vote} variant="outlined" style={{ marginTop: "2vh" }}>
+                   Vote
+                </Button>
+            </FormControl>
+
         </div>
+
     );
+
+
 }
