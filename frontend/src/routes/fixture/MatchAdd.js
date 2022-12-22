76038626c4e8c2839e57507760a1b9ff80c2f0c5
Ege Metin <egemetin@sabanciuniv.edu>
2022-12-23 00:29:23 +0300

    Time Picker is added to Match Add
    For selecting the time, time picker is adjusted

--- frontend/src/routes/fixture/MatchAdd.js
+++ frontend/src/routes/fixture/MatchAdd.js
@@ -12,6 +12,7 @@ import {
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import axios from "axios";
 
 export default function MatchAdd() {
@@ -92,15 +93,14 @@ export default function MatchAdd() {
         //         console.log(err);
         //     });
     };
-
+    console.log(date.getMonth());
     return (
         <div className="matchAddContainer">
             {
                 <div style={{ flexDirection: "column" }}>
                     <h3>Add a New Match</h3>
-                    <div className="textForm">
-                        <div className="textFieldGroup">
-                            {/* {FIELDS.map(({ field, type, value, func }) => {
+                    <div className="textFieldGroup">
+                        {/* {FIELDS.map(({ field, type, value, func }) => {
                                     return (
                                         <div className="textField" key={field}>
                                             <p className="text">{field}</p>
@@ -117,105 +117,111 @@ export default function MatchAdd() {
                                     );
                                 })} */}
 
-                            <p>Teams</p>
-                            <FormControl sx={{ m: 1, minWidth: 300 }}>
-                                <InputLabel id="demo-simple-select-helper-label">
-                                    Team1
-                                </InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-helper-label"
-                                    id="demo-simple-select-helper"
-                                    value={team1}
-                                    label="Team Supported"
-                                    onChange={(event) =>
-                                        setTeam1(event.target.value)
-                                    }
-                                >
-                                    {teams !== [] &&
-                                        teams.map((team, index) => {
-                                            return (
-                                                <MenuItem
-                                                    value={team}
-                                                    key={index.toString()}
-                                                >
-                                                    {team}
-                                                </MenuItem>
-                                            );
-                                        })}
-                                </Select>
-                            </FormControl>
-
-                            <FormControl sx={{ m: 1, minWidth: 300 }}>
-                                <InputLabel id="demo-simple-select-helper-label">
-                                    Team2
-                                </InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-helper-label"
-                                    id="demo-simple-select-helper"
-                                    value={team2}
-                                    label="Team Supported"
-                                    onChange={(event) =>
-                                        setTeam2(event.target.value)
-                                    }
-                                >
-                                    {teams !== [] &&
-                                        teams.map((team, index) => {
-                                            return (
-                                                <MenuItem
-                                                    value={team}
-                                                    key={index.toString()}
-                                                >
-                                                    {team}
-                                                </MenuItem>
-                                            );
-                                        })}
-                                </Select>
-                            </FormControl>
-
-                            <p>Referee</p>
-                            <FormControl sx={{ m: 1, minWidth: 300 }}>
-                                <InputLabel id="demo-simple-select-helper-label">
-                                    Referee
-                                </InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-helper-label"
-                                    id="demo-simple-select-helper"
-                                    value={referee}
-                                    label="Team Supported"
-                                    onChange={(event) =>
-                                        setReferee(event.target.value)
-                                    }
-                                >
-                                    <MenuItem value={""}>
-                                        No referee yet
-                                    </MenuItem>
-                                    {referees &&
-                                        referees.map((referee, index) => (
+                        <p>Teams</p>
+                        <FormControl sx={{ m: 1, minWidth: 300 }}>
+                            <InputLabel id="demo-simple-select-helper-label">
+                                Team1
+                            </InputLabel>
+                            <Select
+                                labelId="demo-simple-select-helper-label"
+                                id="demo-simple-select-helper"
+                                value={team1}
+                                label="Team Supported"
+                                onChange={(event) =>
+                                    setTeam1(event.target.value)
+                                }
+                            >
+                                {teams !== [] &&
+                                    teams.map((team, index) => {
+                                        return (
                                             <MenuItem
+                                                value={team}
                                                 key={index.toString()}
-                                                value={referee}
                                             >
-                                                {referee}
+                                                {team}
                                             </MenuItem>
-                                        ))}
-                                </Select>
-                            </FormControl>
+                                        );
+                                    })}
+                            </Select>
+                        </FormControl>
 
-                            <p>Date</p>
-                            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                <DatePicker
-                                    label="Date"
-                                    value={date}
-                                    onChange={(newValue) => {
-                                        setDate(newValue);
-                                    }}
-                                    renderInput={(params) => (
-                                        <TextField {...params} />
-                                    )}
-                                />
-                            </LocalizationProvider>
-                            <p>Time</p>
-                        </div>
+                        <FormControl sx={{ m: 1, minWidth: 300 }}>
+                            <InputLabel id="demo-simple-select-helper-label">
+                                Team2
+                            </InputLabel>
+                            <Select
+                                labelId="demo-simple-select-helper-label"
+                                id="demo-simple-select-helper"
+                                value={team2}
+                                label="Team Supported"
+                                onChange={(event) =>
+                                    setTeam2(event.target.value)
+                                }
+                            >
+                                {teams !== [] &&
+                                    teams.map((team, index) => {
+                                        return (
+                                            <MenuItem
+                                                value={team}
+                                                key={index.toString()}
+                                            >
+                                                {team}
+                                            </MenuItem>
+                                        );
+                                    })}
+                            </Select>
+                        </FormControl>
+
+                        <p>Referee</p>
+                        <FormControl sx={{ m: 1, minWidth: 300 }}>
+                            <InputLabel id="demo-simple-select-helper-label">
+                                Referee
+                            </InputLabel>
+                            <Select
+                                labelId="demo-simple-select-helper-label"
+                                id="demo-simple-select-helper"
+                                value={referee}
+                                label="Team Supported"
+                                onChange={(event) =>
+                                    setReferee(event.target.value)
+                                }
+                            >
+                                <MenuItem value={""}>No referee yet</MenuItem>
+                                {referees &&
+                                    referees.map((referee, index) => (
+                                        <MenuItem
+                                            key={index.toString()}
+                                            value={referee}
+                                        >
+                                            {referee}
+                                        </MenuItem>
+                                    ))}
+                            </Select>
+                        </FormControl>
+
+                        <p>Date</p>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker
+                                label="Date"
+                                value={date}
+                                onChange={(newValue) => setDate(newValue)}
+                                renderInput={(params) => (
+                                    <TextField {...params} />
+                                )}
+                            />
+                        </LocalizationProvider>
+
+                        <p>Time</p>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <TimePicker
+                                label="Basic example"
+                                value={time}
+                                onChange={(newValue) => setTime(newValue)}
+                                renderInput={(params) => (
+                                    <TextField {...params} />
+                                )}
+                            />
+                        </LocalizationProvider>
                     </div>
                     <Button onClick={addMatch} style={{ marginTop: "2vh" }}>
                         Add Match
