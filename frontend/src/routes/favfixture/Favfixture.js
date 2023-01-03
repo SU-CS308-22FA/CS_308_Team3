427122b36897c97ee5ab86ea5975ef95c891040b
ozgekarasu <58443917+ozgekarasu@users.noreply.github.com>
2023-01-03 18:24:01 +0100

    Mark favorite
    Mark favorite completed as the user click on mark favorite tab, then s/he can select star and go that match details. In that part the user can select this match with "Mark" button which redirects to profile page. Later the user can update his/her favorite match in profile.

---
+++ frontend/src/routes/favfixture/Favfixture.js
@@ -0,0 +1,185 @@
+import { useContext, useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { sortDates } from "../../functions/helpers";
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+
+import "./fixture.scss";
+import { UserContext } from "../../contexts/userContext";
+
+export default function Fixture() {
+    const { user } = useContext(UserContext);
+    const [matches, setMatches] = useState(null);
+    const [week, setWeek] = useState(1);
+    const [isLoading, setIsLoading] = useState(true);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios
+            .get("/match/" + week)
+            .then((res) => {
+                const matchesWoOrder = res.data.matches;
+                // setMatches(matchesWoOrder);
+                // console.table(matchesWoOrder);
+                const sortedMathces = sortDates(matchesWoOrder);
+                setMatches(sortedMathces);
+            })
+            .finally(() => setIsLoading(false))
+            .catch((err) => console.log(err));
+    }, [week]);
+
+    const goToAddMatch = () => {
+        navigate("add-match");
+    };
+
+    const goNextWeek = () => {
+        week < 38 && setWeek((week) => week + 1);
+    };
+
+    const goPrevWeek = () => {
+        week > 1 && setWeek((week) => week - 1);
+    };
+
+    return (
+        <div>
+            <h3>Fixture of the Week</h3>
+            <div className="row" style={{ justifyContent: "space-between" }}>
+                <h3 style={{ color: "red" }}>Week {week}</h3>
+                {user?.userType === "TFF" && (
+                    <Button
+                        color="primary"
+                        variant="outlined"
+                        size="medium"
+                        className="button"
+                        onClick={goToAddMatch}
+                    >
+                        Add a match
+                    </Button>
+                )}
+            </div>
+            <div className="fixture">
+                <div className="weekChanger">
+                    <ArrowCircleLeftIcon
+                        onClick={goPrevWeek}
+                        style={{ height: "35px", width: "auto" }}
+                    />
+                    <p style={{ margin: "0px 10px", fontSize: "22px" }}>
+                        {week}/38
+                    </p>
+                    <ArrowCircleRightIcon
+                        onClick={goNextWeek}
+                        style={{ height: "35px", width: "auto" }}
+                    />
+                </div>
+                <div className="wrapContainer">
+                    {matches &&
+                        !isLoading &&
+                        matches.map(
+                            (
+                                {
+                                    _id,
+                                    team1,
+                                    logo1,
+                                    team2,
+                                    logo2,
+                                    date,
+                                    time,
+                                    referee,
+                                },
+                                index
+                            ) => {
+                                return (
+                                    <div
+                                        className="match"
+                                        key={index}
+                                        onClick={() => {
+                                            navigate(`/tracking/${_id}`);
+                                        }}
+                                    >
+                                        <div className="matchInfo">
+                                            <p style={{ flex: "1" }}>{team1}</p>
+                                            <img
+                                                className="fixtureTeamLogo"
+                                                src={logo1}
+                                                alt={team1}
+                                                style={{ flex: "1" }}
+                                            />
+                                            <div id="datetime">
+                                                <p
+                                                    style={{
+                                                        fontWeight: "bold",
+                                                        margin: "0vw 0.8vw",
+                                                        flex: "1",
+                                                    }}
+                                                >
+                                                    {date}
+                                                </p>
+                                                <p
+                                                    style={{
+                                                        fontWeight: "bold",
+                                                        margin: "0vw 0.8vw",
+                                                        flex: "1",
+                                                    }}
+                                                >
+                                                    {time}
+                                                </p>
+                                            </div>
+                                            <img
+                                                className="fixtureTeamLogo"
+                                                src={logo2}
+                                                alt={team2}
+                                                style={{ flex: "1" }}
+                                            />
+                                            <p style={{ flex: "1" }}>{team2}</p>
+                                        </div>
+                                        <div className="referee">
+                                            {referee === "" ? (
+                                                user?.userType === "TFF" ? (
+                                                    <Button
+                                                        color="primary"
+                                                        variant="outlined"
+                                                        size="medium"
+                                                        className="button"
+                                                    >
+                                                        Assign a referee
+                                                    </Button>
+                                                ) : (
+                                                    <p>No referee assigned</p>
+                                                )
+                                            ) : (
+                                                <p style={{ fontSize: 18 }}>
+                                                    Referee: {referee}
+                                                </p>
+                                            )}
+                                        </div>
+                                        <div class="rate">
+                                    <input onClick={() => {
+                                            navigate(`/tracking/${_id}`);
+                                        }} type="radio" id="star5" name="rate" value="5" />
+                                    <label for="star5" title="Add this match to favourite">5 stars</label>
+                                    </div>
+                                    </div>
+                                
+                                );
+                            }
+                        )}
+                    {(matches === null || isLoading) && <h3>Loading...</h3>}
+                </div>
+            </div>
+            <footer>
+      			<p><b>Turkish Football Federation 2022.</b></p>
+				<p>Services</p>
+				  <p><a href="/">Fixture</a></p>
+				  <p><a href="/referees">Referees</a></p>
+				  <p><a href="/teams">Teams</a></p>
+				  <p><a href="/profile">Profile</a></p>
+				  <p><a href="/faq">FAQ</a></p>
+				  <p><a href="/topratedreferee">Top Rated Referee</a></p>
+				  <p><a href="/topratedmatch">Top Rated Match</a></p>
+				  <p><a href="/sitemap.xml" target="_blank">Sitemap</a></p>
+    			</footer>
+        </div>
+    );
+}
